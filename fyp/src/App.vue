0b945afe116caca15a48c2b47d1dcda4b8eb083d
jakeluvme <jaslyn.zhu.2021@smu.edu.sg>
2024-02-03 03:08:12 +0800

    vue create

---
+++ fyp/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import TSH_home from '@/components/ATRS_home';
+const props = defineProps(['ATRS_home']);
+</script>
+
+<template>
+  <div>
+    <h2>Available Jobs:</h2>
+    <ul>
+      <li v-for="job in jobs" :key="job.title">
+        <strong>{{ job.title }}</strong> - {{ job.location }} | {{ job.type }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+</style>
