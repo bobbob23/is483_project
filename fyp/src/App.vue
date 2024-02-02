fb986031c2c40832b8b936e207f07ba6654b390a
jakeluvme <jaslyn.zhu.2021@smu.edu.sg>
2024-02-03 03:23:13 +0800

    fixed some stuff

--- fyp/src/App.vue
+++ fyp/src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
-import TSH_home from '@/components/ATRS_home';
+import ATRS_home from '@/components/ATRS_home';
 const props = defineProps(['ATRS_home']);
 </script>
 
 <template>
-  <div>
-    <h2>Available Jobs:</h2>
-    <ul>
-      <li v-for="job in jobs" :key="job.title">
-        <strong>{{ job.title }}</strong> - {{ job.location }} | {{ job.type }}
-      </li>
-    </ul>
+  <div id="app">
+    <HomePage />
   </div>
 </template>
 
