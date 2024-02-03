fb972e3e61bb49a37017b6462ddf5c1768aaad7b
jakeluvme <jaslyn.zhu.2021@smu.edu.sg>
2024-02-03 19:26:19 +0800

    banner

---
+++ fyp/src/components/ATRSHome.vue
@@ -0,0 +1,33 @@
+<template>
+<h1>Careers at TSH</h1>
+<div>
+    <ul>
+        <li v-for="jobs in jobs" :key="job.title">
+            <strong>{{ job.title }}</strong> - {{ job.location }} - {{ job.type }} - {{ job.department }}
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobs: []
+        };
+    },
+    mounted() {
+        this.fetchJobs();
+    },
+    methods: {
+        async fetchJobs(){
+            try {
+            const response = await fetch('/jobs.json');
+            this.jobs = await response.json();
+            } catch (error) {
+            console.error('Error fetching jobs:', error);
+            }
+        }
+    }
+} 
+</script>
---
+++ fyp/src/components/Banner.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="banner-photo">
+    <div class="banner-box">
+      <h1>{{ msg }}</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  color: whitesmoke;
+  font-size: 2.6rem;
+  position: relative;
+  font-family: 'Inika-Regular'
+}
+
+.banner-photo {
+  background: url('../assets/tsh-banner.jpg');
+  height: 30vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.banner-box {
+  box-sizing: border-box;
+  padding: 20px;
+  border: 8px solid whitesmoke;
+  text-align: center;
+  position: absolute
+}
+
+</style>
