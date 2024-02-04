3d497b002f3986860690d4a3e30b96ce38f2d04c
Deborah How <hyh.deb@gmail.com>
2024-02-04 21:38:13 +0800

    delete node_modules

--- web-scraping/indeed_scraper/indeed_scraper/items.py
+++ web-scraping/indeed_scraper/indeed_scraper/items.py
@@ -8,5 +8,7 @@ import scrapy
 
 class IndeedScraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    jobKey = scrapy.Field()
+    jobTitle = scrapy.Field()
+    jobDescription = scrapy.Field()
+ 
