bf1f42eaeaf8a260fa1fe58c90f1e34c561a92eb
Deborah How <hyh.deb@gmail.com>
2024-02-07 11:18:28 +0800

    filter functionality added

--- TSH_Application/frontend/src/components/ATRSHome.vue
+++ TSH_Application/frontend/src/components/ATRSHome.vue
@@ -31,20 +31,22 @@
         <div class="row">
             <div class="col-3">
                 <h4>Experience Level</h4>
-                <Listbox class="w-auto" v-model="selectedExperience" :options="experienceLevel" style="" />
+                <Listbox class="w-full" v-model="selectedExperience" :options="experienceLevel"
+                    @click="reloadListings(selectedExperience, selectedCountry)" />
+                <h4 class="mt-3">Location</h4>
+                <Listbox class="w-full" v-model="selectedCountry" :options="countries" style="padding: 0px;"
+                    @click="reloadListings(selectedExperience, selectedCountry)" />
             </div>
             <div class="col-1">
             </div>
             <div class="col-8">
-                <Card v-for="(job, index) in jobs" :key="job.title" style="margin-bottom: 20px; margin-left: -35px; width: 45rem;"
-                @click="viewRoleListing()"
-                @mouseenter="hover[index]=true"
-                @mouseleave="hover[index]=false"
-                class="div"
-                :class="{ 'div-hover': hover[index] }">
+                <Card v-for="(job, index) in jobs" :key="job.title"
+                    style="margin-bottom: 20px; margin-left: -35px; width: 45rem;" @click="viewRoleListing(job)"
+                    @mouseenter="hover[index] = true" @mouseleave="hover[index] = false" class="div"
+                    :class="{ 'div-hover': hover[index] }">
                     <template #title>{{ job.title }}</template>
                     <template #content>
-                        <p class=" m-0">
+                        <p class="m-0">
                             {{ job.location }} - {{ job.type }} - {{ job.department }}
                         </p>
                     </template>
@@ -58,15 +60,16 @@
 import Toolbar from 'primevue/toolbar';
 import Button from 'primevue/button';
 import Card from 'primevue/card';
-import Listbox from 'primevue/listbox';
-
+import Listbox from 'primevue/listbox'
 
 export default {
     data() {
         return {
             jobs: [],
             selectedExperience: "",
-            experienceLevel: ["Internship", "Entry-Level"],
+            experienceLevel: ["Internship", "Entry-Level", "Experienced"],
+            selectedCountry: "",
+            countries: ["Singapore", "Malaysia"],
             searchItem: "",
             hover: [],
         };
@@ -80,8 +83,25 @@ export default {
             })
     },
     methods: {
-        viewRoleListing() {
-            console.log("hello")
+        viewRoleListing(job) {
+            alert(job.title + " job listing")
+        },
+        reloadListings(selectedExperience, selectedCountry) {
+            fetch('jobs.json')
+                .then(res => res.json())
+                .then(data => {
+                    if (!selectedExperience && !selectedCountry) {
+                        this.jobs = data;
+                    } else if (selectedExperience && selectedCountry) {
+                        this.jobs = data.filter(job => {
+                            return job.type === selectedExperience && job.location === selectedCountry;
+                        });
+                    } else if (selectedCountry) {
+                        this.jobs = data.filter(job => job.location === selectedCountry);
+                    } else {
+                        this.jobs = data.filter(job => job.type === selectedExperience);
+                    }
+                })
         }
     },
 }
