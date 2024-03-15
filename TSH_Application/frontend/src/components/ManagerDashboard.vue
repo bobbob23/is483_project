6c1e872d966b0c153cc1d3003924c250a802401a
Deborah How <hyh.deb@gmail.com>
2024-03-15 15:46:49 +0800

    added funnelchart to manager dashboard and added manager tab to navbar

--- TSH_Application/frontend/src/components/ManagerDashboard.vue
+++ TSH_Application/frontend/src/components/ManagerDashboard.vue
@@ -2,21 +2,58 @@
     <HRNavBar />
     <div class="container">
         <h3 class="p-5">Welcome Back, Manager</h3>
-        
+        <div class="d-flex justify-content-center">
+            <highcharts class="hc" :options="funnelChartOptions" style="width: 60%">
+            </highcharts>
+        </div>
     </div>
 </template>
 
 <script>
 import HRNavBar from "./HRNavBar.vue"
 import Highcharts from 'highcharts'
-import exportingInit from 'highcharts/modules/exporting'
+import loadFunnel from 'highcharts/modules/funnel';
 
-exportingInit(Highcharts)
+loadFunnel(Highcharts);
 
 export default {
-    components:{
+    components: {
         HRNavBar
     },
+    data() {
+        return {
+            funnelChartOptions: {
+                chart: {
+                    type: 'pyramid'
+                },
+                title: {
+                    text: ""
+                }, 
+                plotOptions: {
+                    series: {
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name}</b> ({point.y:,.0f})',
+                            softConnector: true
+                        },
+                        // center: ['40%', '60%'],
+                        // neckWidth: '30%',
+                        // neckHeight: '25%',
+                        width: '80%'
+                    }
+                },
+                series: [{
+                    name: '',
+                    data: [
+                        ['Unprocessed', 8000],
+                        ['Shortlisted', 4064],
+                        ['Interviewing', 1987],
+                        ['Successful', 900],
+                    ]
+                }],
+            }
+        }
+    }
 
 }
 
